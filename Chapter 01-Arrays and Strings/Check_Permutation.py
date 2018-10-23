5a5de9ae67e0c1c8b418bb6aedd69da18b656877
Yasaman Rajaee <yasamanrajaee92@gmail.com>
2018-10-23 17:50:31 +0200

    1-1 Completed 1-2 Started

---
+++ Chapter 01-Arrays and Strings/Check_Permutation.py
@@ -0,0 +1,17 @@
+# Describe what it means for two strings to be permutations of each other.
+# Now, look at that definition you provided.
+# Can you check the strings against that definition?
+
+# ***** Hints *****
+# 1) Describe what it means for two strings to be permutations of each other.
+#       Now, look at that definition you provided. Can you check the strings against that definition?
+# 2) There is one solution that is O(N log N) time. Another solution uses some space, but is O(N) time.
+# 3) Could a hash table be useful?
+# 4) Two strings that are permutations should have the same characters, but in different orders.
+#       Can you make the orders the same?
+
+
+# *** Questions ***
+# 1)  Is permutation comparison case sensitive?
+# 2) Is white space significant?
+
--- Chapter 01-Arrays and Strings/Is-Unique.py
+++ Chapter 01-Arrays and Strings/Is-Unique.py
@@ -1,5 +1,21 @@
+# *** Is Unique ***
+# Implement an algorithm to determine if a string has all unique characters.
+# What if you cannot use additional data structures?
+
+# ***** Hints *****
+# 1) Try a hash table
+# 2) Could a bit vector be useful?
+# 3) Can you solve it in O(N log N)time?
+#      What might a solution like that look like?
+
+
+# *** Questions ***
+# 1)  Is the string  an ASCII string or a Unicode string?
+
 
 def is_unique(input_string):
+    if len(input_string) > 128:  # If we assume the string is ASCII
+        return False
     letters = {}
     for letter in input_string:
         if letter in letters:
@@ -10,3 +26,17 @@ def is_unique(input_string):
 
 if __name__ == "__main__":
     print(is_unique(input('Please Enter Your String:')))
+
+
+# The time complexity for this code is O(n), where n is the length of the string. The space complexity is O(l)
+# If we can't use additional data structures, we can do the following:
+
+# 1. Compare every character of the string to every other character of the string.
+# This will take O(n2) time and 0(1) space.
+
+# 2. If we are allowed to modify the input string,
+# we could sort the string in O(n log(n)) time
+# and then linearly check the string for neighboring characters that are identical.
+# Careful, though: many sorting algorithms take up extra space.
+
+
